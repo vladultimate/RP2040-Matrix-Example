73e895ae7ceaf1a809d0b864c1725a736d8d3f48
vladultimate <128429156+vladultimate@users.noreply.github.com>
2024-01-08 05:21:50 +0200

    Add files via upload
    """""""""""""""""""""""""""""""""""""""
    appearing and disappearing circle
    """""""""""""""""""""""""""""""""""""""

---
+++ disappearing_circle.py
@@ -0,0 +1,28 @@
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep_ms
+
+pixels = NeoPixel(Pin(16), 25)
+
+big_bouble = [1,2,3,5,9,10,14,15,19,21,22,23]
+
+def clear(neopixels):
+    neopixels.fill((0, 0, 0))
+    neopixels.write()
+
+def circle():
+    for pixel in big_bouble:
+        pixels[pixel] = (5,5,20)  
+        pixels.write()
+        sleep_ms(75)
+
+    sleep_ms(75)
+
+    for pixel in big_bouble[::-1]:
+        pixels[pixel] = (0,0,0)
+        pixels.write()
+        sleep_ms(75)
+
+while True:
+    circle()
+    sleep_ms(100)
